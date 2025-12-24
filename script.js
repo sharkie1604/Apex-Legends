5b723f63a2995beda37d18735dff570ec84092a4
Aryan Chauhan <aryanchauhan1604@Aryans-MacBook-Pro.local>
2025-12-25 02:42:57 +0530

    changed the entire project

---
+++ script.js
@@ -0,0 +1,34 @@
+// JS code for the movement of Going Merry. It will register a scroll and
+//then change the properties of ship. If GSAP is unable to load from the
+// source provided it will display the error
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (typeof gsap !== "undefined") {
+    gsap.registerPlugin(ScrollTrigger);
+    
+    const anm = gsap.timeline({
+        scrollTrigger:{
+            trigger: ".parallax-container",
+            scrub: 1
+        }
+    });
+    anm.to(".ship", {x:"471vw"});
+    anm.to(".ship-message", {
+        opacity: 0,
+        duration: 0.0
+    }, 0.2);
+    anm.to(".ship-message", {
+        opacity: 1,
+        duration: 0.2
+    },0.4);
+    // gsap.to(".ship", {
+    //    x: "467vw",
+    //    scrollTrigger: {
+    //      trigger: ".parallax-container",
+    //      scrub: 1
+    //    }
+    // });
+  } else {
+    console.error("GSAP did not load correctly.");
+  }
+});
